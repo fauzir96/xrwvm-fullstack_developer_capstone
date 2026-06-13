22a948975d195c4fde8fa547ea476842707acdce
FauziCoursera <ahmadfauzirohman@outlook.com>
2026-06-12 20:13:18 -0400

    Django admin

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path(route='logout', view=views.logout_user, name='logout'),
     path(route='register', view=views.registration, name='register'),
     # path for dealer reviews view
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 
     # path for add a review view
 

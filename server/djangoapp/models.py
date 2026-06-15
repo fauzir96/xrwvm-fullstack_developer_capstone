942da84dc34a5612f0d1c84ba4e71c04a40b8a8d
fauzir96 <150206556+fauzir96@users.noreply.github.com>
2026-06-15 17:28:26 +0700

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -13,22 +13,23 @@ class CarMake(models.Model):
     def __str__(self):
         return self.name
 
+
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE) 
-    
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+
     name = models.CharField(max_length=100)
-    
+
     # Dealer ID
-    dealer_id = models.IntegerField() 
-    
+    dealer_id = models.IntegerField()
+
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
         ('SUV', 'SUV'),
         ('WAGON', 'Wagon'),
     ]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    
-    # Year (IntegerField) dengan validasi nilai minimum 2015 dan maksimum 2023 
+
+    # Year (IntegerField) dengan validasi nilai minimum 2015 dan maksimum 2023
     year = models.IntegerField(
         default=2023,
         validators=[
@@ -39,4 +40,3 @@ class CarModel(models.Model):
 
     def __str__(self):
         return f"{self.car_make.name} - {self.name}"
-

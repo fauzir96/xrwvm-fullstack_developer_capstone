03b26388d8d488956964f770c639c76c9495a7a1
fauzir96 <150206556+fauzir96@users.noreply.github.com>
2026-06-15 20:16:25 +0700

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,9 +29,12 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://ahmadfauzir-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai']
-CSRF_TRUSTED_ORIGINS = ['https://ahmadfauzir-8000.theianext-0-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
-'https://ahmadfauzir-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+                 'https://ahmadfauzir-8000.theianext-0-labs-prod-misc-'
+                 'tools-us-east-0.proxy.cognitiveclass.ai']
+CSRF_TRUSTED_ORIGINS = ['https://ahmadfauzir-8000.theianext-0-labs-prod-'
+                        'misc-tools-us-east-0.proxy.cognitiveclass.ai',
+                        'https://ahmadfauzir-8000.theiadockernext-1-labs-'
+                        'prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
